01ed1af69a3816cc1ca6ebbde0e4e82d1afa49d9
Alissa Likavec <alissalikavec@gmail.com>
2017-03-21 17:50:32 -0700

    Updating TimerCtrl, and tasks

--- dist/scripts/controllers/TimerCtrl.js
+++ dist/scripts/controllers/TimerCtrl.js
@@ -1,12 +1,16 @@
 (function(){
   function TimerCtrl($scope, TimeOut, Tasks, $firebase){
     this.timeOut = TimeOut;
-    $scope.tasks = Tasks.all;
+    this.tasks = Tasks.all;
 
-    $scope.createTask = function(){
-      Tasks.create($scope.task).then(function(data){
-        $scope.task.title = ''
-      })
+    this.createTask = function(taskTitle){
+      var taskObj = {
+        title: taskTitle,
+        createdAt: firebase.database.ServerValue.TIMESTAMP
+      }
+
+      Tasks.create(taskObj);
+      this.title = '';
     };
   }
 
